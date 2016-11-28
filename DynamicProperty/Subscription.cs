1ef066734f25b3c25128ea48e63338f485b2f4d5
Dmitry Sergeev <ru.sergeev@gmail.com>
2016-11-28 05:11:44 -0600

    Subscription doc

--- DynamicProperty/Subscription.cs
+++ DynamicProperty/Subscription.cs
@@ -2,13 +2,22 @@
 
 namespace Developer.Test
 {
+    /// <summary>
+    /// disposable Subscription object
+    /// </summary>
     sealed class Subscription : IDisposable
     {
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="unsubscribe"> action to call on dispose </param>
         public Subscription(Action<Subscription> unsubscribe)
         {
             _unsubscribe = unsubscribe;
         }
-
+        /// <summary>
+        /// to support IDisposable
+        /// </summary>
         public void Dispose()
         {
             _unsubscribe(this);
