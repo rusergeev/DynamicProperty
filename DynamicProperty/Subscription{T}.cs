8350e484520a9fde5f9660d94191caa780bbdb78
Dmitry Sergeev <ru.sergeev@gmail.com>
2016-11-28 05:08:32 -0600

    Doc subscription factory

--- DynamicProperty/Subscription{T}.cs
+++ DynamicProperty/Subscription{T}.cs
@@ -5,20 +5,35 @@ using System.Collections.Generic;
 
 namespace Developer.Test
 {
+    /// <summary>
+    /// Subscription Factory
+    /// </summary>
+    /// <typeparam name="T"> callback type </typeparam>
     sealed class Subscription<T>: IEnumerable<T>
     {
+        /// <summary>
+        /// created a subscription
+        /// </summary>
+        /// <param name="callback"> callback to call when notify </param>
+        /// <returns> Disposable object </returns>
         public IDisposable Create(T callback)
         {
             var subscription = new Subscription(Unsubscribe);
             _callbacks[subscription] = callback;
             return subscription;
         }
-
+        /// <summary>
+        /// To enumerate throw callback
+        /// </summary>
+        /// <returns> enumerator </returns>
         public IEnumerator<T> GetEnumerator()
         {
             return _callbacks.Values.GetEnumerator();
         }
-
+        /// <summary>
+        /// supports enumeration
+        /// </summary>
+        /// <returns> enumerator </returns>
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
