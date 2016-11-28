669e201a66de122c6ad52e7a69778757b7244b9d
Dmitry Sergeev <ru.sergeev@gmail.com>
2016-11-27 19:29:47 -0600

    separate dependency source and target

--- DynamicProperty/DependencySourceProperty.cs
+++ DynamicProperty/DependencySourceProperty.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.Remoting.Contexts;
@@ -31,32 +30,5 @@ namespace Developer.Test
             }
             set { _value.Value = value; }
         }
-    }
-
-    class DependencyTargetProperty<T> : Wrapper<T>, IDependencyTarget
-    {
-        public DependencyTargetProperty(Func<T> read, Action<T> write) : base(new DependencySourceProperty<T>(read, write))
-        {
-        }
-        public override T Value
-        {
-            get
-            {
-                    var targets = ThreadStack.Instance.Current;
-                    targets.Push(this);
-                    var value = _value.Value;
-                    var check = targets.Pop();
-                    Debug.Assert(check == this, "Thread stack is broken.");
-
-                return value;
-            }
-            set { _value.Value = value; }
-        }
-
-        public void SubscribeTo<TSource>(DependencySourceProperty<TSource> source)
-        {
-            source.Subscribe(value => { });
-        }
-
     }
 }
---
+++ DynamicProperty/DependencyTargetProperty.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Developer.Test
+{
+    class DependencyTargetProperty<T> : Wrapper<T>, IDependencyTarget
+    {
+        public DependencyTargetProperty(Func<T> read, Action<T> write) : base(new DependencySourceProperty<T>(read, write))
+        {
+        }
+        public override T Value
+        {
+            get
+            {
+                var targets = ThreadStack.Instance.Current;
+                targets.Push(this);
+                var value = _value.Value;
+                var check = targets.Pop();
+                Debug.Assert(check == this, "Thread stack is broken.");
+
+                return value;
+            }
+            set { _value.Value = value; }
+        }
+
+        public void SubscribeTo<TSource>(DependencySourceProperty<TSource> source)
+        {
+            source.Subscribe(value => { });
+        }
+
+    }
+}
