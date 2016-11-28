5f1195b30f55f0e119a2ca502aedb57bc8715707
Dmitry Sergeev <ru.sergeev@gmail.com>
2016-11-28 05:47:30 -0600

    IDependency Target doc

--- DynamicProperty/CalculatedProperty.cs
+++ DynamicProperty/CalculatedProperty.cs
@@ -2,8 +2,6 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Developer.Test
 {
--- DynamicProperty/IDependencyTarget.cs
+++ DynamicProperty/IDependencyTarget.cs
@@ -1,7 +1,14 @@
 ﻿namespace Developer.Test
 {
+    /// <summary>
+    /// Dependency consumer -  <see cref="CalculatedProperty{T}"/>
+    /// </summary>
     interface IDependencyTarget
     {
+        /// <summary>
+        /// to subscribe to dependency source
+        /// </summary>
+        /// <param name="source"> a dependency source - <see cref="BasicProperty{T}"/></param>
         void SubscribeTo(IDependencySource source);
     }
 }
