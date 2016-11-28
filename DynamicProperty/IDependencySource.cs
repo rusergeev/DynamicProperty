b897eb1424c278225d0b137e59d7f4e2adcd187c
Dmitry Sergeev <ru.sergeev@gmail.com>
2016-11-28 05:51:04 -0600

    Dependency source docs

--- DynamicProperty/IDependencySource.cs
+++ DynamicProperty/IDependencySource.cs
@@ -2,9 +2,20 @@
 
 namespace Developer.Test
 {
+    /// <summary>
+    /// ependency source - <see cref="BasicProperty{T}"/>
+    /// </summary>
     interface IDependencySource
     {
+        /// <summary>
+        ///  Subscribes a notify callback to this basic dynamic property
+        /// </summary>
+        /// <param name="notify"> property changed callback </param>
+        /// <returns></returns>
         IDisposable Subscribe(Action notify);
+        /// <summary>
+        /// Notify all consumers
+        /// </summary>
         void NotifyAllTargets();
     }
 }
