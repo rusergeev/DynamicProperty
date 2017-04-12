4a0936a2743a62d729542c628c8503377d6bf992
Dmitry Sergeev <ru.sergeev@gmail.com>
2017-04-12 14:43:41 -0500

    Put interfaces to different files

---
+++ DynamicProperty/IDependency.cs
@@ -0,0 +1,10 @@
+﻿using JetBrains.Annotations;
+
+namespace DynamicProperty
+{
+    interface IDependency
+    {
+        void Support([NotNull] IDependent dependent);
+        void DoesNotSupport([NotNull] IDependent dependent);
+    }
+}
---
+++ DynamicProperty/IDependent.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+
+namespace DynamicProperty
+{
+    interface IDependent
+    {
+        void DependsOn([NotNull] IDependency dependency);
+        void DoesNotDependOn([NotNull] IDependency dependency);
+        void Recalculate();
+    }
+}
